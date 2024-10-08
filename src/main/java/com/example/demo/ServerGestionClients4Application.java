90c4916c34b330bdfe9a4ae5c1deae1abc655425
Youssef-Elmessaoudi <elmessaoudiyoussef2023@gmail.com>
2024-10-08 22:54:49 +0100

    Premier

---
+++ src/main/java/com/example/demo/ServerGestionClients4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGestionClients4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerGestionClients4Application.class, args);
+	}
+
+}
